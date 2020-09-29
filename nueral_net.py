65294b1e1e32184f2e5a652468650487dd9c1662
fadilparves <fadil.parves@gmail.com>
2020-09-29 21:29:17 +0800

    added init func

---
+++ nueral_net.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+import struct
+
+class NNClassificationModel:
+
+    def __init__(self, n_classes, n_features, n_hidden_units=30, l1=0.0, l2=0.0, epochs=1000, learning_rate=0.01, n_batches=1, random_seed=None):
+        if random_seed:
+            np.random.seed(random_seed)
+        self.n_classes = n_classes
+        self.n_features = n_features
+        self.n_hidden_units = n_hidden_units
+        self.w1, self.w2 = self._init_weights()
+        self.l1 = l1
+        self.l2 = l2
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.n_batches = n_batches
