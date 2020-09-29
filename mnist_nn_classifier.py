1996188c88aaed4a20360264f969e8e200ab9618
fadilparves <fadil.parves@gmail.com>
2020-09-29 22:09:54 +0800

    prepared the test script

---
+++ mnist_nn_classifier.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from nueral_net import NNClassificationModel
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+rcParams['figure.figsize'] = 14, 8
+
