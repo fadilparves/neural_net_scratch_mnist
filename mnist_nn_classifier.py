baca4643e03b36294e6c91b937b88610738fa3d4
fadilparves <fadil.parves@gmail.com>
2020-09-29 22:20:21 +0800

    prepared all plots method

--- mnist_nn_classifier.py
+++ mnist_nn_classifier.py
@@ -21,3 +21,49 @@ def read_data(images_path, labels_path):
         images = np.fromfile(p, dtype=np.uint8).reshape(len(labels), 784)
     
     return images, labels
+
+def plot_error(model):
+    plt.plot(range(len(model.error_)), model.error_)
+    plt.ylabel('Error')
+    plt.xlabel('Epochs')
+    plt.show()
+
+def plot_image(i, predictions_array, true_labels, images):
+  predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  
+  plt.imshow(img.reshape(28,28), cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+  
+  plt.xlabel("{} {:2.0f}% ({})".format(CLASS_NAMES[predicted_label],
+                                100*np.max(predictions_array),
+                                CLASS_NAMES[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1]) 
+  predicted_label = np.argmax(predictions_array)
+ 
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+  
+def plot_image_grid(X_test, y_test, y_hat, num_rows=5, num_cols=3):
+  num_images = num_rows*num_cols
+  plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+  for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, y_hat, y_test, X_test)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, y_hat,  y_test)
