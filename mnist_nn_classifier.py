077f6ba29617fa035a114ac58bfbd191326f4fa3
fadilparves <fadil.parves@gmail.com>
2020-09-29 22:18:58 +0800

    working on test case

--- mnist_nn_classifier.py
+++ mnist_nn_classifier.py
@@ -3,6 +3,7 @@ import seaborn as sns
 from pylab import rcParams
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import struct
 from nueral_net import NNClassificationModel
 
 sns.set(style='whitegrid', palette='muted', font_scale=1.5)
@@ -11,3 +12,12 @@ rcParams['figure.figsize'] = 14, 8
 CLASS_NAMES = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal',      'Shirt',   'Sneaker',  'Bag',   'Ankle boot']
 
+def read_data(images_path, labels_path):
+    with open(labels_path, 'rb') as p:
+        magic, n = struct.unpack('>II', p.read(8))
+        labels = np.fromfile(p, dtype=np.uint8)
+    with open(images_path, 'rb') as p:
+        magic, num, rows, cols = struct.unpack(">IIII", p.read(16))
+        images = np.fromfile(p, dtype=np.uint8).reshape(len(labels), 784)
+    
+    return images, labels
